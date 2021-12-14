8602ed66292fe580dea1f4c101a79ee06bdb255f
Vadim Priymachuk <vadym.mr@gmail.com>
2021-12-15 00:59:36 +0200

    fix sonar bugs

--- resources/views/reports/html.blade.php
+++ resources/views/reports/html.blade.php
@@ -8,18 +8,18 @@
     @else
         @if ($observation->airportName) <br> {{ $observation->airportName }} @endif
         @if ($observation->date) <br> {{ $observation->date }} @endif
-        @if ($observation->wind) <br><b>Wind:</b> {{ $observation->wind }} @endif
-        @if ($observation->visibility) <br><b>Visibility:</b> {{ $observation->visibility }} @endif
-        @if ($observation->skyConditions) <br><b>Sky conditions:</b> {{ $observation->skyConditions }} @endif
-        @if ($observation->temperature) <br><b>Temperature:</b> {{ $observation->temperature }} @endif
-        @if ($observation->weather) <br><b>Weather:</b> {{ $observation->weather }} @endif
-        @if ($observation->heatIndex) <br><b>Heat index:</b> {{ $observation->heatIndex }} @endif
-        @if ($observation->windchill) <br><b>Windchill:</b> {{ $observation->windchill }} @endif
-        @if ($observation->dewPoint) <br><b>Dew point:</b> {{ $observation->dewPoint }} @endif
-        @if ($observation->relativeHumidity) <br><b>Relative humidity:</b> {{ $observation->relativeHumidity }} @endif
-        @if ($observation->pressureAltimeter) <br><b>Pressure altimeter:</b> {{ $observation->pressureAltimeter }} @endif
-        @if ($observation->pressureTendency) <br><b>Pressure tendency:</b> {{ $observation->pressureTendency }} @endif
-        @if ($observation->ob) <br><b>ob:</b> {{ $observation->ob }} @endif
-        @if ($observation->cycle) <br><b>cycle:</b> {{ $observation->cycle }} @endif
+        @if ($observation->wind) <br><strong>Wind:</strong> {{ $observation->wind }} @endif
+        @if ($observation->visibility) <br><strong>Visibility:</strong> {{ $observation->visibility }} @endif
+        @if ($observation->skyConditions) <br><strong>Sky conditions:</strong> {{ $observation->skyConditions }} @endif
+        @if ($observation->temperature) <br><strong>Temperature:</strong> {{ $observation->temperature }} @endif
+        @if ($observation->weather) <br><strong>Weather:</strong> {{ $observation->weather }} @endif
+        @if ($observation->heatIndex) <br><strong>Heat index:</strong> {{ $observation->heatIndex }} @endif
+        @if ($observation->windchill) <br><strong>Windchill:</strong> {{ $observation->windchill }} @endif
+        @if ($observation->dewPoint) <br><strong>Dew point:</strong> {{ $observation->dewPoint }} @endif
+        @if ($observation->relativeHumidity) <br><strong>Relative humidity:</strong> {{ $observation->relativeHumidity }} @endif
+        @if ($observation->pressureAltimeter) <br><strong>Pressure altimeter:</strong> {{ $observation->pressureAltimeter }} @endif
+        @if ($observation->pressureTendency) <br><strong>Pressure tendency:</strong> {{ $observation->pressureTendency }} @endif
+        @if ($observation->ob) <br><strong>ob:</strong> {{ $observation->ob }} @endif
+        @if ($observation->cycle) <br><strong>cycle:</strong> {{ $observation->cycle }} @endif
     @endif
 @endforeach
